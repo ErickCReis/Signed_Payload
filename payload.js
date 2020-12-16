5d72a91ce5369d45b1a4ba57b55d0f53d39bbe1d
ErickCReis <erickcorreareis@gmail.com>
2020-12-16 17:51:32 -0300

    Update database and minor fixes

--- payload.js
+++ payload.js
@@ -1,4 +1,3 @@
-const { v4: uuid } = require('uuid');
 const { sign } = require('./keystore/sign');
 const { keystore } = require('./keystore');
 
@@ -7,13 +6,13 @@ module.exports = {
     if (req.url === '/payload' && res.statusCode === 201 && req.method === 'POST') {
         res.jsonp({url: `localhost:3000/payload/${res.locals.data.id}`});
     } else if (req.method == 'GET' && /.payload*/.test(req.url)) {
-        let signed = await sign(res.locals.data);
-        res.send(await sign(res.locals.data));
+        const signed = await sign(res.locals.data);
+        res.send(signed);
     } else {
       res.send(res.locals.data);
     }
   },
-  async getJwkset(req, res) {
+  async getJwkset(_, res) {
     res.jsonp(keystore.getPublicKeys());
   }
 }
